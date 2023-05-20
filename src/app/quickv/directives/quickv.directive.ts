0625548f2a3bc56942747b2cce391f8556a5d462
Claude Fassinou <dev.claudy@gmail.com>
2023-05-20 20:19:22 +0200

    first commit

---
+++ src/app/quickv/directives/quickv.directive.ts
@@ -0,0 +1,15 @@
+import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: 'form',
+})
+export class QuickvDirective implements AfterViewInit {
+  constructor(private refElement: ElementRef<HTMLElement>) {}
+  ngAfterViewInit(): void {
+    //@ts-ignore
+    const qvForm = new QvForm(this.refElement.nativeElement);
+    qvForm.init({
+      validClass: 'is-valid',
+    });
+  }
+}
---
+++ src/app/quickv/directives/qv-messages.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appQvMessages]'
+})
+export class QvMessagesDirective {
+
+  constructor() { }
+
+}
